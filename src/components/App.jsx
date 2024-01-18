65727e2096346d7e50ad99b55456de7976d90816
vosob <osobskyi.vlad@gmail.com>
2024-01-19 00:39:32 +0200

    q

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,53 @@
-import React, { Component } from 'react';
-import FeeedBack from './FeedBack/FeeedBack';
+import React, { useState } from 'react';
+import { FeedBackStatistics } from 'components/FeedBackStatistics/FeedBackStatistics';
+import { FeedBackOptions } from 'components/FeedBackOptions/FeedBackOptions';
+import { Section } from 'components/Section/Section';
+import { Notification } from 'components/Notification/Notification';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <FeeedBack />
-      </>
-    );
-  }
-}
+const App = () => {
+  const [feedbackState, setFeedbackState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const { good, neutral, bad } = feedbackState;
+  const total = good + neutral + bad;
+  const options = Object.keys(feedbackState);
+
+  const addFeedback = option => {
+    setFeedbackState(prevCounts => ({
+      ...prevCounts,
+      [option]: prevCounts[option] + 1,
+    }));
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    return total ? Math.round((feedbackState.good / total) * 100) : 0;
+  };
+
+  return (
+    <>
+      <Section title={'Please leave feedback'}>
+        <FeedBackOptions options={options} addFeedback={addFeedback} />
+      </Section>
+      {total === 0 ? (
+        <Notification mess="There is no feedback" />
+      ) : (
+        <>
+          <Section title={'Statistics'}>
+            <FeedBackStatistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positiveFeedback={countPositiveFeedbackPercentage()}
+            />
+          </Section>
+        </>
+      )}
+    </>
+  );
+};
 
 export default App;
--- src/components/FeedBack/FeedBack.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Title = styled.section`
-  padding-left: 20px;
-  padding-top: 20px;
-  padding-bottom: 10px;
-  font-size: 24px;
-  font-weight: 700;
-`;
